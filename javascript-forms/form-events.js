ae38a34fb80aad064f7642aee9a91f64d7a3d22e
Justin-Arakaki <Justiniankoi@gmail.com>
2022-02-09 11:33:29 -0800

    moved over functionality oops

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -1,7 +1,6 @@
 const $name = document.querySelector('#user-name');
 const $email = document.querySelector('#user-email');
 const $message = document.querySelector('#user-message');
-const $form = document.querySelector('#contact-form');
 $name.addEventListener('focus', handleFocus);
 $name.addEventListener('blur', handleBlur);
 $name.addEventListener('input', handleInput);
@@ -11,7 +10,6 @@ $email.addEventListener('input', handleInput);
 $message.addEventListener('focus', handleFocus);
 $message.addEventListener('blur', handleBlur);
 $message.addEventListener('input', handleInput);
-$form.addEventListener('submit', handleSubmit);
 
 function handleFocus(event) {
   console.log('focus event fired');
@@ -26,14 +24,4 @@ function handleBlur(event) {
 function handleInput(event) {
   console.log(event.target.name);
   console.log(event.target.value);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const messageData = {};
-  messageData.name = event.target.elements.name.value;
-  messageData.email = event.target.elements.email.value;
-  messageData.message = event.target.elements.message.value;
-  console.log('submit event fired');
-  console.log(messageData);
 }
---
+++ javascript-forms/form-values.js
@@ -0,0 +1,12 @@
+const $form = document.querySelector('#contact-form');
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const messageData = {};
+  messageData.name = event.target.elements.name.value;
+  messageData.email = event.target.elements.email.value;
+  messageData.message = event.target.elements.message.value;
+  console.log('submit event fired');
+  console.log(messageData);
+}

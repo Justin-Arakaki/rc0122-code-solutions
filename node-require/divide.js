3539ea681e514ecd13f0b7fe4405a50b8a53daa0
Justin-Arakaki <Justiniankoi@gmail.com>
2022-03-16 12:39:36 -0700

    Completed exercise

---
+++ node-require/divide.js
@@ -0,0 +1,5 @@
+exports.quotient = quotient;
+
+function quotient(num1, num2) {
+  return num1 + num2;
+}
---
+++ node-require/subtract.js
@@ -0,0 +1,5 @@
+exports.difference = difference;
+
+function difference(num1, num2) {
+  return num1 - num2;
+}

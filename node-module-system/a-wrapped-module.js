d50d2bc0692869ac3074ea9ff7aff6942e87a0f0
Justin-Arakaki <Justiniankoi@gmail.com>
2022-03-16 11:05:16 -0700

    Completed exercise

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,10 @@
+console.log('exports', exports);
+console.log('require', require);
+console.log('module', module);
+console.log('__filename', __filename);
+console.log('__dirname', __dirname);
+console.log('type of ' + 'exports', typeof exports);
+console.log('type of ' + 'require', typeof require);
+console.log('type of ' + 'module', typeof module);
+console.log('type of ' + '__filename', typeof __filename);
+console.log('type of ' + '__dirname', typeof __dirname);

1978d7d61d3d196bfbc4a669247c87c4393ce1bf
Justin-Arakaki <Justiniankoi@gmail.com>
2022-03-29 00:29:36 -0700

    Completed exercise

---
+++ array-filter/main.js
@@ -0,0 +1,23 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(value => value % 2 === 0);
+console.log('evenNumbers:', evenNumbers);
+const overFive = numbers.filter(value => value > 5);
+console.log('overFive:', overFive);
+const startWithE = names.filter(value => value[0] === 'E');
+console.log('startsWithE:', startWithE);
+const haveD = names.filter(value =>
+  value.includes('d') || value.includes('D'));
+console.log('haveD:', haveD);

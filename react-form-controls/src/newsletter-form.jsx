12572283fc84578c1d365ae6dccf48f882d214c1
Justin-Arakaki <Justiniankoi@gmail.com>
2022-04-05 15:45:35 -0700

    Logs state when submitting

--- react-form-controls/src/newsletter-form.jsx
+++ react-form-controls/src/newsletter-form.jsx
@@ -3,26 +3,18 @@ import React from 'react';
 export default class NewsletterForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { email: '', timerID: null };
+    this.state = { email: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({ email: event.target.value });
-    this.logState();
   }
 
   handleSubmit(event) {
     event.preventDefault();
-  }
-
-  logState() {
-    clearTimeout(this.state.timerID);
-    const timer = setTimeout(() => {
-      console.log('state:', this.state);
-    }, 1000);
-    this.setState({ timerID: timer });
+    console.log(this.state);
   }
 
   render() {

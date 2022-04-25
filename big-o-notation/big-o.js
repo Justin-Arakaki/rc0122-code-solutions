9e1d0532351736ef58d6adc6749e9b4f1fb33902
Justin-Arakaki <Justiniankoi@gmail.com>
2022-04-25 16:53:09 -0700

    Mistakes were made

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -21,15 +21,15 @@ function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * 1 = O(1)
+    i < words.length;               // 2 * n = O(1)
     i++                             // 2 * n = O(2n)
   ) {
     const word = words[i];          // 2 * n = O(2n)
     let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * n = O(n)
-      c < i;                        // 2 * n = O(2n)
-      c++                           // 2 * n = O(2n)
+      let c = 0;                    // 1 * n^2 = O(n^2)
+      c < i;                        // 2 * n^2 = O(2n^2)
+      c++                           // 2 * n^2 = O(2n^2)
     ) {
       const comparing = words[c];   // 2 * n^2 = O(2n^2)
       if (comparing === word) {     // 1 * n^2 = O(n^2)
